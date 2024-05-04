eb18b703c635a207d2dfb628e76675984e8f3faa
SayafHameed <sheikhsayaf@gmail.com>
2024-05-04 08:33:07 +0530

    load changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const btn = document.querySelector("button");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
-
+let amount = document.querySelector("input");
 for (let option of SelectOptions) {
   for (code in countryList) {
     let newoption = document.createElement("option");
@@ -19,13 +19,13 @@ for (let option of SelectOptions) {
   option.addEventListener("change", (e) => {
     updateflag(e.target);
   });
-  option.addEventListener('change',(e)=>{
+  option.addEventListener("change", (e) => {
     convert(e);
-  })
+  });
 }
 async function convert(e) {
   e.preventDefault();
-  let amount = document.querySelector("input");
+  
   let amtValue = amount.value;
 
   if (amtValue === "" || amtValue < 1) {
@@ -41,7 +41,9 @@ async function convert(e) {
 
   const rate = data.rates[toCurr.value];
   let finalAmount = amtValue * rate;
-  msg.innerText = `${amtValue} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCurr.value}`;
+  msg.innerText = `${amtValue} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${
+    toCurr.value
+  }`;
 }
 
 const updateflag = (ele) => {
@@ -54,17 +56,15 @@ const updateflag = (ele) => {
   img.src = newsrc;
 };
 
-
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   convert(e);
 });
-window.addEventListener('load',(e)=>{
+window.addEventListener("load", (e) => {
   convert(e);
-})
+});
 
-// console.log(fromCurr.value+"-->"+toCurr.value);
-// btn.addEventListener("click", async (e) => {
+
 //   e.preventDefault();
 //   let amount = document.querySelector("input");
 //   let amtValue = amount.value;
@@ -85,16 +85,35 @@ window.addEventListener('load',(e)=>{
 //   let finalAmount = amtValue * rate;
 //   msg.innerText = `${amtValue} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
 // });
-let exchange=document.querySelector("i");
-exchange.addEventListener('click',(e)=>{
-  let temp=toCurr.value;
-  toCurr.value=fromCurr.value;
-  fromCurr.value=temp;
-  
-let i1=document.getElementById('fromimg')
-i1.src=`https://flagsapi.com/${countryList[fromCurr.value]}/flat/64.png`;
-let i2=document.getElementById('toimg')
-i2.src=`https://flagsapi.com/${countryList[toCurr.value]}/flat/64.png`
+let exchange = document.querySelector("i");
+// console.log(fromCurr.value + "-->" + toCurr.value);
 
-  // console.log(fromCurr.value+"-->"+toCurr.value);
-})
+exchange.addEventListener("click",async (e) => {
+  
+  // console.log(e.target);
+  let temp = toCurr.value;
+  toCurr.value = fromCurr.value;
+  fromCurr.value = temp;
+  // console.log(fromCurr.value + "-->" + toCurr.value);
+  let amtValue=amount.value;
+  const URL = `https://api.frankfurter.app/latest?&from=${toCurr.value}&to=${fromCurr.value}`;
+
+  let response = await fetch(URL);
+
+  let data = await response.json();
+  // console.log(data);
+
+  const rate = data.rates[fromCurr.value];
+  let finalAmount = amtValue / rate;
+  msg.innerText = `${amtValue} ${fromCurr.value} = ${finalAmount.toFixed(3)} ${
+    toCurr.value}`
+
+
+  // msg.innerText=` = ${finalAmount.toFixed(2)} ${toCurr.value}=${amtValue} ${fromCurr.value}`
+  let i1 = document.getElementById("fromimg");
+  i1.src = `https://flagsapi.com/${countryList[fromCurr.value]}/flat/64.png`;
+  let i2 = document.getElementById("toimg");
+  i2.src = `https://flagsapi.com/${countryList[toCurr.value]}/flat/64.png`;
+
+ 
+});
